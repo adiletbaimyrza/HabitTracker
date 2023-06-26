cdfd0111091a437f2b512f61395e9bf42fad16d1
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-06-26 15:55:25 +0200

    added routes

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html", title="Habit Tracker - Home")
+
+@app.route("/add")
+def add_habit():
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
