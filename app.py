ed6dddad81c17fb427ce4a8810f20e8e2e1603d7
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-06-26 17:17:00 +0200

    last update

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+habits = ["Test Habit 1", "Test Habit 2"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.form == "POST":
+        habit = request.form.get("habit")
+        habits.append(habit)
+    
     return render_template("add_habit.html", title="Habit Tracker - Add Habit")
