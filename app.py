dfa436c56d656b467dd9a0f278cf277d793225ef
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-06-26 19:12:08 +0200

    update app

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+import datetime
 
 app = Flask(__name__)
 habits = ["Test Habit 1", "Test Habit 2"]
 
+
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.datetime):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    
+    return {"date_range": date_range}
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+    date_str = request.args.get("date")
+    
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+        
+    return render_template("index.html",
+                           habits=habits,
+                           title="Habit Tracker - Home",
+                           selected_date=selected_date)
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
@@ -13,4 +33,6 @@ def add_habit():
         habit = request.form.get("habit")
         habits.append(habit)
     
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template("add_habit.html",
+                           title="Habit Tracker - Add Habit",
+                           selected_date=datetime.date.today())
